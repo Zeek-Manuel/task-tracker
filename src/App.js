d22fc35d92813e31be9f83cee19922b6124fa86f
ZG Website <ekwesijnr@gmail.com>
2023-04-19 14:55:54 +0000

    adding and recording task

--- src/App.js
+++ src/App.js
@@ -27,6 +27,12 @@ const App = () => {
     ]
   )
 
+  const addTask = (task) => {
+    const id = Math.floor(Math.random()*1000) +1
+    const newTask = {id, ...task}
+    setTasks([...tasks, newTask])
+  }
+
   const deleteTask = (id)=>{
     setTasks(tasks.filter((task) => task.id !== id))
   }
@@ -38,7 +44,7 @@ const App = () => {
   return (
     <div className="container">
     <Header/>
-    <AddTask />
+    <AddTask onAdd={addTask} />
     {tasks.length > 0 ? (
     <Tasks tasks={tasks} 
     onDelete={deleteTask} onToggle={toggleReminder}/>) :
